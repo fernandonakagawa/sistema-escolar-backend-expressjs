e0fdba5e48f35266952828257b165ae8a4990cf1
Fernando Nakagawa <fernandonakagawa@outlook.com>
2022-10-25 20:31:18 +0000

    Update app.js and package.json

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const express = require("express");
 const routes = require("./routes");
 //const indexRouter = require("./routes/index");
 //const usersRouter = require("./routes/users");
-
+const cors = require("cors");
 const app = express();
-
+app.use(cors());
 //app.use(logger("dev"));
 //app.use(express.json());
 //app.use(express.urlencoded({ extended: false }));
